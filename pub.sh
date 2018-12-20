1eb12adab620fb61c4c5bab5179855a0bf766c51
Russ Buchanan <russ@havn.fun>
2018-12-19 22:33:14 -0500

    Increase Java heap size

--- pub.sh
+++ pub.sh
@@ -7,4 +7,4 @@ echo getting rid of .DS_Store files since they gum up the igpublisher....
 find . -name '.DS_Store' -type f -delete
 echo running publisher
 cd /guide-in
-java -jar /publisher/org.hl7.fhir.igpublisher.jar -ig ig.json -watch -tx $NA
+java  -Xms256m -Xmx2096m -jar /publisher/org.hl7.fhir.igpublisher.jar -ig ig.json -watch -tx $NA
