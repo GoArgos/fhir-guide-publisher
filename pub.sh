692207274bca83ad089ad7b7c6392f2f7ef8a1a6
Russ Buchanan <russ@havn.fun>
2019-01-29 21:48:48 -0500

    Remove step to cleanup output

--- pub.sh
+++ pub.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo getting publisher
 curl -O http://build.fhir.org/org.hl7.fhir.igpublisher.jar
-echo getting rid of old output
-rm /guide-out/*
 echo getting rid of .DS_Store files since they gum up the igpublisher....
 find . -name '.DS_Store' -type f -delete
 echo running publisher
