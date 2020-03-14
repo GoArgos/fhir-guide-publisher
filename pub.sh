aaab245388c099a4afe46fb3ef003020a7aec817
Russell Buchanan <15872739+russbuchanan@users.noreply.github.com>
2020-03-14 12:46:24 -0400

    Add support for new framework

--- pub.sh
+++ pub.sh
@@ -2,7 +2,19 @@
 file="/guide/_genUpdatePublisher.sh"
 if [ -f "$file" ]; then
   cd /guide
-  ./_genUpdatePublisher.sh
+  /guide/_genUpdatePublisher.sh
 else
-  echo "Unexpected framework or no guide to publish."
+  file="/guide/_genonce.sh"
+  if [ -f "$file" ]; then
+    file="/guide/input-cache/org.hl7.fhir.publisher.jar"
+    cd /guide
+    if [ -f "$file" ]; then
+      /guide/_genonce.sh
+    else
+      /guide/_updatePublisher.sh
+      /guide/_genonce.sh
+    fi
+  else
+    echo "Unexpected framework or no guide to publish."
+  fi
 fi
