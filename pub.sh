8bde385628ce7cd19d67be7ac49aecc0bb2acc6b
Russell Buchanan <15872739+russbuchanan@users.noreply.github.com>
2020-04-26 12:37:44 -0400

    make shell scripts executable at runtime

--- pub.sh
+++ pub.sh
@@ -2,15 +2,19 @@
 file="/guide/_genUpdatePublisher.sh"
 if [ -f "$file" ]; then
   cd /guide
+  chmod +x _genUpdatePublisher.sh
   /guide/_genUpdatePublisher.sh
 else
   file="/guide/_genonce.sh"
   if [ -f "$file" ]; then
     file="/guide/input-cache/org.hl7.fhir.publisher.jar"
     cd /guide
+    chmod +x _genonce.sh
     if [ -f "$file" ]; then
+
       /guide/_genonce.sh
     else
+      chmod +x _updatePublisher.sh
       /guide/_updatePublisher.sh
       /guide/_genonce.sh
     fi
